29cb401931240eff12f4384f3be4d45512b79d69
Faith-Nchifor <52848633+Faith-Nchifor@users.noreply.github.com>
2020-03-02 10:50:47 +0100

    Updates

---
+++ precache-manifest.2b1e495617e0f2fc67c5ecec96a10435.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b189d062aaf0ff0738c699d2e6c98179",
+    "url": "/Faith-Nchifor/test.github.io/index.html"
+  },
+  {
+    "revision": "5902a226f83c500fe828",
+    "url": "/Faith-Nchifor/test.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1d82f6806564a41ca99b",
+    "url": "/Faith-Nchifor/test.github.io/static/js/2.e1e040c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Faith-Nchifor/test.github.io/static/js/2.e1e040c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5902a226f83c500fe828",
+    "url": "/Faith-Nchifor/test.github.io/static/js/main.ce9a9bf1.chunk.js"
+  },
+  {
+    "revision": "43127d40d95b41b00a6e",
+    "url": "/Faith-Nchifor/test.github.io/static/js/runtime-main.cb777dd6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Faith-Nchifor/test.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
